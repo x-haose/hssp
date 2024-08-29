1d2566f6e5cdae29ee236ad9ed7d3dcef7a7eb9c
昊色居士 <xhrtxh@gmail.com>
2024-08-29 22:57:20 +0800

    加解密工具文件
    1、aes256 解密的填充问题
    2、增加aes256 不填充的方法
    3、增加md5、sha256的方法

--- src/hssp/utils/crypto.py
+++ src/hssp/utils/crypto.py
@@ -1,5 +1,39 @@
+from hashlib import md5, sha256
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad, unpad
+
+
+def encrypt_aes_256_cbc(data: bytes, key: bytes, iv: bytes) -> bytes:
+    """
+    aes_256加密 不填充
+    Args:
+        data: 加密的数据
+        key: key
+        iv: iv
+
+    Returns:
+
+    """
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(data)
+    return ciphertext
+
+
+def decrypt_aes_256_cbc(data: bytes, key: bytes, iv: bytes) -> bytes:
+    """
+    aes_256解密 不填充
+    Args:
+        data: 数据
+        key: key
+        iv: iv
+
+    Returns:
+
+    """
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.decrypt(data)
+    return ciphertext
 
 
 def encrypt_aes_256_cbc_pad7(data: bytes, key: bytes, iv: bytes) -> bytes:
@@ -15,9 +49,7 @@ def encrypt_aes_256_cbc_pad7(data: bytes, key: bytes, iv: bytes) -> bytes:
     """
 
     data = pad(data, 16)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = cipher.encrypt(data)
-    return ciphertext
+    return encrypt_aes_256_cbc(data, key, iv)
 
 
 def decrypt_aes_256_cbc_pad7(data: bytes, key: bytes, iv: bytes) -> bytes:
@@ -31,7 +63,41 @@ def decrypt_aes_256_cbc_pad7(data: bytes, key: bytes, iv: bytes) -> bytes:
     Returns:
 
     """
-    data = unpad(data, 16)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = cipher.decrypt(data)
-    return ciphertext
+    data = decrypt_aes_256_cbc(data, key, iv)
+    return unpad(data, AES.block_size)
+
+
+def md5_hash(data: bytes | str, result_type: str = "hex"):
+    """
+    md5 hash
+    Args:
+        data: 数据，字节数据或字符串，字符串或使用utf8编码为字节
+        result_type: 返回类型，默认为16进制字符串，bytes为字节
+
+    Returns:
+        返回经过md5哈希后的值
+    """
+    data = data if isinstance(data, bytes) else data.encode('utf-8')
+    result = md5(data)
+    if result_type == "hex":
+        return result.hexdigest()
+    else:
+        return result.digest()
+
+
+def sha256_hash(data: bytes | str, result_type: str = "hex"):
+    """
+    sha256 hash
+    Args:
+        data: 数据，字节数据或字符串，字符串或使用utf8编码为字节
+        result_type: 返回类型，默认为16进制字符串，bytes为字节
+
+    Returns:
+        返回经过sha256哈希后的值
+    """
+    data = data if isinstance(data, bytes) else data.encode('utf-8')
+    result = sha256(data)
+    if result_type == "hex":
+        return result.hexdigest()
+    else:
+        return result.digest()
